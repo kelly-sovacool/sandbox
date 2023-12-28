c1ee6a56e809feb0c24ceca7b643fd9a793f2c54
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-12-28 13:43:25 -0500

    ci: create docker to build R pkg

--- r.sandbox/R/sandbox.R
+++ r.sandbox/R/sandbox.R
@@ -1,3 +1,12 @@
 greet <- function() {
   print('hello world!')
 }
+
+get_docker_version <- function(event_name, github_ref) {
+  if (event_name == 'release') {
+   version <- github_ref
+  } else {
+   version <- paste(packageVersion('r.sandbox'), github_ref, sep = '_')
+  }
+  return(as.character(version))
+}
